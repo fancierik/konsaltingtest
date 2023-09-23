ca531fbec3791b93f5ddeb1f1f56804de5871c34
fancierik <anblackdiamond@gmail.com>
2023-09-23 15:12:22 +0300

    Update

--- src/main/java/com/example/konsaltingtest/KonsaltingtestApplication.java
+++ src/main/java/com/example/konsaltingtest/KonsaltingtestApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KonsaltingtestApplication {
 
     public static void main(String[] args) {
-
+//jdhgsdjkcfsbsfgseyjcfbsh
         SpringApplication.run(KonsaltingtestApplication.class, args);
     }
 
